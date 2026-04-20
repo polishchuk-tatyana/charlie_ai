c70ae448480389f1f6f02c278d70ec02b56428b9
janecarroll <tatiana.polish23@gmail.com>
2026-04-21 00:37:52 +0300

    update log_handler

--- log_handler.py
+++ log_handler.py
@@ -1,10 +1,16 @@
 import logging
 from pathlib import Path
 
+LOG_DIR = Path(__file__).resolve().parent / "log"
+LOG_FILE = LOG_DIR / "log.log"
+
+LOG_DIR.mkdir(parents=True, exist_ok=True)
+LOG_FILE.touch(exist_ok=True)
+
 logger = logging.getLogger("app")
 logger.setLevel(logging.DEBUG)
 
-handler_ecs = logging.FileHandler(Path("log", "log.log"))
+handler_ecs = logging.FileHandler(LOG_FILE)
 logger.addHandler(handler_ecs)
 
 # handler_stream = logging.StreamHandler()
